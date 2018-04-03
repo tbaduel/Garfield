8e98bbe6f076272de1ef6bc2d23ac3a98ad16862
Thomas Baduel <Thomas@Thomas-PC1.localdomain>
2018-04-03 23:06:45 +0200

    edit opcode

--- src/fr/upem/net/tcp/Opcode.java
+++ src/fr/upem/net/tcp/Opcode.java
@@ -4,6 +4,7 @@ public enum Opcode {
 	ERROR(-1), LOGIN(0), SIGNUP(1), MESSAGE(2), REQUEST(3), FILE(4), IPRESPONSE(98), MESSAGEBROADCAST(99), WHISP_OK(
 			107), LOGIN_OK(103), LOGIN_ERR(104), SIGNUP_OK(105), SIGNUP_ERR(106)
 
+	
 	;
 
 	public final int op;
