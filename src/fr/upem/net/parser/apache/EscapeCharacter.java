8ff67b08a4e075862b27ee87d3c230e094fc53e2
ishafie <ichemshafie@hotmail.fr>
2018-05-01 23:09:08 +0200

    escape char

--- src/fr/upem/net/parser/apache/EscapeCharacter.java
+++ src/fr/upem/net/parser/apache/EscapeCharacter.java
@@ -13,7 +13,7 @@ public class EscapeCharacter {
 		escapeJavaMap.put("\"", "\\\"");
 		escapeJavaMap.put("\\", "\\\\");
 		ESCAPE_JAVA = new AggregateTranslator(new LookupTranslator(Collections.unmodifiableMap(escapeJavaMap)),
-				new LookupTranslator(EntityArrays.JAVA_CTRL_CHARS_ESCAPE), JavaUnicodeEscaper.outsideOf(32, 0x7f));
+				new LookupTranslator(EntityArrays.JAVA_CTRL_CHARS_ESCAPE));
 	}
 
 	public static final String escapeJava(final String input) {
